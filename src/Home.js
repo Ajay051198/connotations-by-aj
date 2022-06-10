dabc3cb55df8ae56b07cb2eb9eb071670be3265d
Ajay051198 <ajay.uom@gmail.com>
2022-06-09 21:20:57 -0400

    landing page

---
+++ src/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './Home.module.css';
+
+function Home() {
+  return (
+    <div>
+      <div className={styles.slate}>
+        <div className={styles.aj}>
+          AJ
+        </div>
+        <div className={styles.slogan}>
+          Daring to be present
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Home from './Home';
+
 import './index.css';
 
 const container = document.getElementById('root');
@@ -12,12 +17,11 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
